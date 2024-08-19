6adb7b2f26ce35597b1eb3053bcc508c76c5b428
1xtAsh <ashraykrsingh321@gmail.com>
2024-08-19 13:16:21 +0530

    sm8350-common: Build missing libraries for Dolby Atmos

--- common.mk
+++ common.mk
@@ -331,6 +331,9 @@ vendor.lineage.livedisplay@2.0-service-sdm
 # Media
 PRODUCT_PACKAGES += \
 libcodec2_hidl@1.0.vendor \
+libcodec2_soft_common.vendor \
+libstagefright_softomx.vendor \
+libstagefright_softomx_plugin.vendor \
 libmm-omxcore \
 libOmxAacEnc \
 libOmxAmrEnc \
